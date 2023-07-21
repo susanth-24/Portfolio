c64a9da55b246fd2c0edf80bfb40c98969eb2485
susanth-lab <68644335+susanth-lab@users.noreply.github.com>
2023-07-21 20:02:23 +0530

    removed bug

--- src/components/Down.jsx
+++ src/components/Down.jsx
@@ -8,45 +8,89 @@ const Down=()=>{
     const[active,setActive]=useState("");
     const[toggle,setToggle]=useState(false);
     return(
-        <nav className="sm:px-16 px-6 w-full flex items-center
-        py-5 top-0 z-20  bg-primary">
-            <div  className="w-full flex justify-between
-            items-center max-w-7xl mx-auto">
-                <Link 
-                to="/"
-                className="flex items-center gap-2"
-                onClick={()=>{
-                    setActive("");
-                    window.scrollTo(0,0);}}>
-                        <img src={logo} alt="logo"
-                        className="w-9 h-9 object-contain rounded-full"/>
-                        <p className="text-white text-[18px] font-semibold cursor-pointer flex">SUSANTH &nbsp;
-                        </p>
-                    </Link>
-                    <ul className="list-none sm:flex flex-row gap-10">
-                        <li>
-                            <a
-                            href="https://github.com/susanth-24/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="fab fa-github text-2xl w-8 h-8 cursor-pointer mr-14"
-                            ></a>
-                            <a
-                            href="https://www.linkedin.com/in/sai-susanth-reddy-sanam-a1b7a9223/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="fab fa-linkedin text-2xl w-8 h-8 cursor-pointer mr-14"
-                            ></a>
-                            <a
-                            href="https://www.instagram.com/susanth_24ig/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="fab fa-instagram text-2xl w-8 h-8 cursor-pointer mr-10"
-                            ></a>
-                        </li>
-                        </ul>
-            </div>
-        </nav>
+        // <nav className="sm:px-16 px-6 w-full flex items-center
+        // py-5 top-0 z-20  bg-primary">
+        //     <div  className="w-full flex justify-between
+        //     items-center max-w-7xl mx-auto">
+        //         <Link 
+        //         to="/"
+        //         className="flex items-center gap-2"
+        //         onClick={()=>{
+        //             setActive("");
+        //             window.scrollTo(0,0);}}>
+        //                 <img src={logo} alt="logo"
+        //                 className="w-9 h-9 object-contain rounded-full"/>
+        //                 <p className="text-white text-[18px] font-semibold cursor-pointer flex">SUSANTH &nbsp;
+        //                 </p>
+        //             </Link>
+        //             <ul className="list-none sm:flex flex-row gap-10">
+        //                 <li>
+        //                     <a
+        //                     href="https://github.com/susanth-24/"
+        //                     target="_blank"
+        //                     rel="noopener noreferrer"
+        //                     className="fab fa-github text-2xl w-8 h-8 cursor-pointer mr-14"
+        //                     ></a>
+        //                     <a
+        //                     href="https://www.linkedin.com/in/sai-susanth-reddy-sanam-a1b7a9223/"
+        //                     target="_blank"
+        //                     rel="noopener noreferrer"
+        //                     className="fab fa-linkedin text-2xl w-8 h-8 cursor-pointer mr-14"
+        //                     ></a>
+        //                     <a
+        //                     href="https://www.instagram.com/susanth_24ig/"
+        //                     target="_blank"
+        //                     rel="noopener noreferrer"
+        //                     className="fab fa-instagram text-2xl w-8 h-8 cursor-pointer mr-10"
+        //                     ></a>
+        //                 </li>
+        //                 </ul>
+        //     </div>
+        // </nav>
+        <nav className="sm:px-16 px-6 w-full flex flex-col sm:flex-row items-center py-5 top-0 z-20 bg-primary">
+        <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+          <Link
+            to="/"
+            className="flex items-center gap-2"
+            onClick={() => {
+              setActive("");
+              window.scrollTo(0, 0);
+            }}
+          >
+            <img src={logo} alt="logo" className="w-9 h-9 object-contain rounded-full" />
+            <p className="text-white text-xl font-semibold cursor-pointer flex">SUSANTH &nbsp;</p>
+          </Link>
+          
+          <ul className="list-none sm:flex flex-row gap-6">
+            <li>
+              <a
+                href="https://github.com/susanth-24/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="fab fa-github text-2xl w-8 h-8 cursor-pointer"
+              ></a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/sai-susanth-reddy-sanam-a1b7a9223/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="fab fa-linkedin text-2xl w-8 h-8 cursor-pointer"
+              ></a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/susanth_24ig/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="fab fa-instagram text-2xl w-8 h-8 cursor-pointer"
+              ></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      
+
     )
 }
 export default Down
